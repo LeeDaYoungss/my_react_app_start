631b5750832f13ddd968a1a980ea032a6eaef3e6
LeeDaYoungss <leedayeonggg@gmail.com>
2024-05-27 13:51:07 +0900

    Update Mynav.js

--- src/components/Mynav.js
+++ src/components/Mynav.js
@@ -6,6 +6,7 @@ class Mynav extends Component{
     let data = this.props.data;
     let i = 0;
     while(i<data.length){
+      // 반복분을 사용할 땐 key 라는 값이 있어야함
       lists.push(<li key={data[i].id}><a href="">{data[i].title}</a></li>);
       i++;
     }
