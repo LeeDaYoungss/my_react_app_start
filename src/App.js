6d2f9ce6521f24abf4f33d8fb55282dad5f7e674
LeeDaYoungss <leedayeonggg@gmail.com>
2024-05-27 12:16:52 +0900

    myheader title desc 출력

--- src/App.js
+++ src/App.js
@@ -10,11 +10,51 @@ function App() {
 }
 */
 
+class Myheader extends Component{
+  render(){
+    console.log(this);
+    return (
+      <header>
+        <h1 class="logo">{this.props.title}</h1>
+        <p>{this.props.desc}</p>
+      </header>
+    );
+  }
+}
+
+class Mynav extends Component{
+  render(){
+    return (
+      <nav>
+        <ul>
+          <li><a href="">HTML</a></li>
+          <li><a href="">CSS</a></li>
+          <li><a href="">JavaScript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+class Myarticle extends Component{
+  render(){
+    return (
+      <section>
+        <article>
+          <h2>HTML</h2>
+          <p>Hypertext markup language</p>
+        </article>
+      </section>
+    );
+  }
+}
+
 class App extends Component{
   render(){
     return (
       <div className="App">
-        Hello, World!
+        <Myheader title="React" desc="Single Page Application"/>
+        <Mynav/>
+        <Myarticle/>
       </div>
     );
   }
